6ca72ab7b10fe31bff99ec301a5067a384ddff3b
Samuel Ige <samuelige@Samuels-MacBook-Pro.local>
2026-03-28 02:31:16 +0100

    refactor: update project structure and dependencies
    - Updated package.json to include new dependencies and updated versions.
    - Removed reset-project script and its associated file.
    - Refactored _layout.tsx to simplify layout management using expo-router.
    - Deleted explore.tsx and its related components to streamline the app.
    - Simplified index.tsx to provide a basic starting point for the app.
    - Removed unused components such as animated-icon, app-tabs, and themed components.
    - Cleaned up theme-related constants and styles.
    - Removed web-specific components and styles to focus on core functionality.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,6 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+export default function RootLayout() {
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
